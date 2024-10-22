941236fe2e29e6a318e0fdf76a9533ed321f1efd
giovani-wahl <giovaniwahl@gmail.com>
2024-10-22 14:30:34 -0300

    stock entity

--- src/main/java/com/giovaniwahl/investAPI/entities/Stock.java
+++ src/main/java/com/giovaniwahl/investAPI/entities/Stock.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -25,4 +26,41 @@ public class Stock {
 
     @OneToMany(mappedBy = "id.stock")
     private Set<Asset> assets = new HashSet<>(); // Lista de ativos na carteira
+
+    public Stock() {}
+
+    public Stock(UUID id, String nome, User client, Instant creationDate) {
+        this.id = id;
+        this.nome = nome;
+        this.client = client;
+        this.creationDate = creationDate;
+    }
+
+    public UUID getId() {return id;}
+    public void setId(UUID id) {this.id = id;}
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public User getClient() {return client;}
+    public void setClient(User client) {this.client = client;}
+
+    public Instant getCreationDate() {return creationDate;}
+    public void setCreationDate(Instant creationDate) {this.creationDate = creationDate;}
+
+    public Set<Asset> getAssets() {return assets;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Stock stock = (Stock) o;
+        return Objects.equals(id, stock.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }

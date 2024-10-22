656e438ed26e15d318f10711f55c6a1eaba008a3
giovani-wahl <giovaniwahl@gmail.com>
2024-10-22 14:12:22 -0300

    entitie user

--- src/main/java/com/giovaniwahl/investAPI/entities/User.java
+++ src/main/java/com/giovaniwahl/investAPI/entities/User.java
@@ -18,4 +18,23 @@ public class User {
 
     @OneToMany(mappedBy = "client" )
     private Set<Stock> stocks = new HashSet<>();
+
+    public User() {}
+
+    public User(UUID id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UUID getId() {return id;}
+    public void setId(UUID id) {this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public Set<Stock> getStocks() {return stocks;}
 }

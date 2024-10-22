ea986d95893712192bd5eda259a0ee4abfa0ac04
giovani-wahl <giovaniwahl@gmail.com>
2024-10-22 14:23:28 -0300

    assetpk entity

--- src/main/java/com/giovaniwahl/investAPI/entities/AssetPK.java
+++ src/main/java/com/giovaniwahl/investAPI/entities/AssetPK.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+
 @Embeddable
 public class AssetPK {
     @ManyToOne
@@ -13,4 +15,28 @@ public class AssetPK {
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
+
+    public AssetPK() {}
+
+    public Stock getStock() {return stock;}
+    public void setStock(Stock stock) {this.stock = stock;}
+
+    public Product getProduct() {return product;}
+    public void setProduct(Product product) {this.product = product;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AssetPK assetPK = (AssetPK) o;
+        return Objects.equals(stock, assetPK.stock) && Objects.equals(product, assetPK.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(stock);
+        result = 31 * result + Objects.hashCode(product);
+        return result;
+    }
 }
--- src/main/java/com/giovaniwahl/investAPI/entities/Category.java
+++ src/main/java/com/giovaniwahl/investAPI/entities/Category.java
@@ -2,6 +2,7 @@ package com.giovaniwahl.investAPI.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -24,4 +25,18 @@ public class Category {
 
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
